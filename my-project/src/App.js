3cc546be93e416c8dccaba5177964df306d71eb0
Itstf <favarelli.thaiza@gmail.com>
2022-09-27 11:35:18 -0300

    commit

---
+++ my-project/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from '../src/components/Navbar'
+
+function App() {
+  return (
+    <>
+      {/* <h1 className="text-4xl text-center"> testando </h1> */}
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+// #323232
+// #8a76d7 - purple-500
+// #f67e09 - amber-500
+// https://tailwindcss.com/docs/background-color |-> color
+
+// https://www.youtube.com/watch?v=TVQnhcVFTVs |-> 24:13min
+
+import React from 'react'
+import {MenuIcon, XIcon} from '@heroicons/react/outline'
+
+const Navbar = () => {
+    return (
+        <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadlow-lg'>
+            <div className='px-2 flex justify-between items-center w-full h-full'>
+                <div className='flex items-center'>
+                    <h1 className='text-3xl font-bold mr-4 sm:text-4xl text-amber-500'>UNDA</h1>
+                    <ul className='hidden md:flex'>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Support</li>
+                        <li>abccc</li>
+                    </ul>
+                </div>
+                <div className='hidden md:flex pr-4'>
+                    <button className='border-none bg-transparent text-black mr-4'> Sign In </button>
+                    <button className='px-8 py-3'> Sign Up </button>
+                </div>
+                <div className='md:hidden'>
+                    <MenuIcon className='w-5' />
+                </div>
+            </div>
+
+            <ul className='absolute bg-zinc-200 w-full px-8'> {/* hamburguer */}
+                <li className='border-b-2 border-zinc-300 w-full'>Home</li>
+                <li className='border-b-2 border-zinc-300 w-full'>About</li>
+                <li className='border-b-2 border-zinc-300 w-full'>Support</li>
+                <li className='border-b-2 border-zinc-300 w-full'>abccc</li>
+                <div className='flex flex-col my-4'>
+                    <button className='bg-transparent text-purple-500 px-8 py-3 mb-4'> Sign In</button>
+                    <button className='px-8 py-3'> Sign Up</button>
+                </div>
+            </ul>
+
+        </div>
+    )
+}
+export default Navbar
