70d1d456be9032d4284868644ca4e98b846060c6
Itstf <favarelli.thaiza@gmail.com>
2022-10-25 16:58:30 -0300

    responsividade

--- my-project/src/components/Application.jsx
+++ my-project/src/components/Application.jsx
@@ -14,21 +14,21 @@ const Application = () => {
                 </div>
 
                 <div className='grid md:grid-cols-3 gap-1 px-2 text-center'>
-                    <div className='border py-8 rounded-xl shadow-xl '>
+                    <div className='border py-8 rounded-xl shadow-xl'>
                         <p className='text-2xl font-bold text-amber-500 hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-purple-500 cursor-default transition duration-300 ease-in-out'> Login </p>                        
-                        <div className='w-48 ml-24 lg:ml-28 shadow-2xl items-center mt-5'>
+                        <div className='w-48 ml-24 lg:ml-28 shadow-2xl items-center mt-5 sm:ml-64 md:ml-16'>
                             <img className='w-full hover:scale-105 transition duration-300 ease-in-out shadow-lg shadow-purple-500/50 grayscale hover:grayscale-0' src={imgApp} alt="/" />
                         </div>
                     </div>
                     <div className='border py-8 rounded-xl shadow-lg shadow-purple-500/50'>
                         <p className='text-2xl font-bold text-amber-500 hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-purple-500 cursor-default transition duration-300 ease-in-out'> Extract </p>
-                        <div className='w-48 ml-24 lg:ml-28 shadow-2xl items-center mt-5'>
+                        <div className='w-48 ml-24 lg:ml-28 shadow-2xl items-center mt-5 sm:ml-64 md:ml-16'>
                             <img className='w-full hover:scale-105 transition duration-300 ease-in-out grayscale hover:grayscale-0' src={imgExtract} alt="/" />
                         </div>
                     </div>
                     <div className='border py-8 rounded-xl shadow-xl'>
                         <p className='text-2xl font-bold text-amber-500 hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-purple-500 cursor-default transition duration-300 ease-in-out'> Profile </p>
-                        <div className='w-48 ml-24 lg:ml-28 shadow-2xl items-center mt-5'>
+                        <div className='w-48 ml-24 lg:ml-28 shadow-2xl items-center mt-5 sm:ml-64 md:ml-16'>
                             <img className='w-full hover:scale-105 transition duration-300 ease-in-out shadow-lg shadow-purple-500/50 grayscale hover:grayscale-0' src={imgProfile} alt="/" />
                         </div>
                     </div>
