05224b4ee410758369fd814181bedc7ce498659b
Itstf <favarelli.thaiza@gmail.com>
2022-10-27 11:28:41 -0300

    themedark

--- my-project/src/components/Sign_in.jsx
+++ my-project/src/components/Sign_in.jsx
@@ -52,7 +52,7 @@ const Sign_in = (props) => {
                                     <input id='terms' aria-describedby='terms' type='checkbox' className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800' required=''/>
                                 </div>
                                 <div class='ml-3 text-sm'>
-                                    <label for='terms' className='font-light text-gray-500 dark:text-gray-300'>Remember-me</label>
+                                    <label for='terms' className='font-light text-gray-500 dark:text-gray-300'>Remember</label>
                                 </div>
                             </div>
 
