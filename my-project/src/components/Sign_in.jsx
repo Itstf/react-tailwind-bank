ac97ea00772612f96bae099e7420d80db84f0425
Itstf <favarelli.thaiza@gmail.com>
2022-11-04 16:06:49 -0300

    padding.

--- my-project/src/components/Sign_in.jsx
+++ my-project/src/components/Sign_in.jsx
@@ -47,7 +47,7 @@ const Sign_in = () => {
                                 <div class='flex items-center h-5'>
                                     <input id='terms' aria-describedby='terms' type='checkbox' className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800' required=''/>
                                 </div>
-                                <div class='ml-3 text-sm'>
+                                <div class='ml-3 text-sm mb-5'>
                                     <label for='terms' className='font-light text-gray-500 dark:text-gray-300'>Remember</label>
                                 </div>
                             </div>
