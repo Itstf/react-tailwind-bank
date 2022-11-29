4d25fe34393f550c99124dbafdc1f974ec507bf2
Silva Thaiza (CaP/ETS) <sth7ca@bosch.com>
2022-11-29 15:10:46 -0300

    GETinfoDjango

--- my-project/src/components/User.jsx
+++ my-project/src/components/User.jsx
@@ -1,31 +1,20 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import Sign_in from './Sign_in'
 
 const User = () => { 
 
-    // objetivo: Puxar variavel para o campo da web 
+    //// objetivo: Puxar variavel para o campo da web
+    const [items, setItems] = useState([])
+    const [ld, setLd] = useState([])
 
-    const [cliente, setCliente] = useState('')
-    const [agencia, setAgencia] = useState('')
-    const [tipo, setTipo] = useState('')
-    const [saldo, setSaldo] = useState('')
-
-    async function info()
-    {
-        let item={'cliente':cliente, 'agencia':agencia, 'tipo':tipo, 'saldo':saldo}
-
-        let endereco = 'http://127.0.0.1:8000/conta/'
-        let result = await fetch(endereco,{
-            method: 'GET',
-            headers:{
-                'Content-Type':'application/json',
-                'Accept':'application/json'
-            },
-            body:JSON.stringify(item)
+    useEffect (() => {
+        fetch('http://127.0.0.1:8000/conta/')
+        .then(res => res.json())
+        .then ((result) => {
+            setLd(true);
+            setItems(result);
         })
-        result = await result.json()
-        console.log(result)
-        localStorage.setItem('user-info',JSON.stringify(result))
-    }
+    })
 
   return (
     <div className='overflow-hidden bg-white shadow sm:rounded-lg mx-auto py-24 px-14 lg:max-w-7xl'>
@@ -45,15 +34,21 @@ const User = () => {
                 </div>
                 <div className='bg-gray-100 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                     <dt className='text-sm font-medium text-gray-500'>Account</dt>
-                    <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>1010005151</dd>
+                    {items.map(item => (
+                        <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0' key={item.id}>{item.agencia}</dd>
+                    ))}
                 </div>
                 <div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                     <dt className='text-sm font-medium text-gray-500'>Card</dt>
-                    <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>True</dd>
+                    {items.map(item => (
+                        <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0' key={item.id}>{item.tipo}</dd>
+                    ))}
                 </div>
                 <div className='bg-gray-100 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                     <dt className='text-sm font-medium text-gray-500'>Value</dt>
-                    <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>$120,000</dd>
+                    {items.map(item => (
+                        <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0' key={item.id}>{item.saldo}</dd>
+                    ))}
                 </div>
             </dl>
         </div>
